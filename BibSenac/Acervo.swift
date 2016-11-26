4791097c1491b3d74f1e8a8bcf0cdfb709e7b8c8
natanaelribeiro <natanaelribeiro17@gmail.com>
2016-11-26 13:18:23 -0200

    Criação das Views

---
+++ BibSenac/Acervo.swift
@@ -0,0 +1,28 @@
+//
+//  Acervo.swift
+//  BibSenac
+//
+//  Created by Natanael Ribeiro on 26/11/16.
+//  Copyright © 2016 Edm Senac Projeto IOS. All rights reserved.
+//
+
+import Foundation
+
+struct Acervo {
+    let _id: String?
+    let _senac: String?
+    let dstaq: String?
+    let chamd: String?
+    let aPrin : String?
+    let aArtg : String?
+    let aScdr : String?
+    let pbcao : String?
+    let fsico : String?
+    let rsumo : String?
+    let notas : String?
+    let assun : String?
+    let url : String?
+    let pdico : String?
+    let serie : String?
+    let adicionado : String?
+}
---
+++ BibSenac/Disponibilidade.swift
@@ -0,0 +1,21 @@
+//
+//  Disponibilidade.swift
+//  BibSenac
+//
+//  Created by Natanael Ribeiro on 26/11/16.
+//  Copyright © 2016 Edm Senac Projeto IOS. All rights reserved.
+//
+
+import Foundation
+
+struct Disponibilidade {
+    let local : String?
+    let parte : String?
+    let disponib : String?
+    let localiza: String?
+    let dt_empre : String?
+    let dt_devol : String?
+    let exemplar : String?
+    let colecao : String?
+}
+
