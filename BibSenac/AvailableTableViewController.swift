8c283ae8b3069a1facfa011ca25919a3b4eeaf40
natanaelribeiro <natanaelribeiro17@gmail.com>
2016-12-03 09:05:56 -0200

    AvailableTableViewController inserido

--- BibSenac/AvailableTableViewController.swift
+++ BibSenac/AvailableTableViewController.swift
@@ -5,91 +5,83 @@
 //  Created by Natanael Ribeiro on 26/11/16.
 //  Copyright © 2016 Edm Senac Projeto IOS. All rights reserved.
 //
-
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class AvailableTableViewController: UITableViewController {
-
+    
+    var _senac:String?
+    let URL_AVAILABLE = "http://pelletteria.net.br/edm/modulos/data_mining/mAcervo_disponivel.php?_senac="
+    var availables = [Disponibilidade]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        _senac="25542"
+        
+        checkAvailableFromServer(idSenac: _senac!) { (availables, error: Error?) in
+            if error == nil {
+                // get obj
+                //self.tableView.reloadData()
+                print("deu certo")
+                print(availables!)
+            } else {
+                print(error.debugDescription)
+                print("deu Errado")
+            }
+            
+        }
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
+    
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 0
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return 0
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        
         // Configure the cell...
-
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    
+    
+    func checkAvailableFromServer(idSenac:String , callback: @escaping( _ availables:[Disponibilidade]?, _ error: Error?) -> () ) {
+        
+        Alamofire.request(URL_AVAILABLE + idSenac).responseJSON { response in
+            let json = JSON(data: response.data!)
+            print(json)
+            
+            for (_, subJSON): (String, JSON) in json {
+                let available = Disponibilidade(local: subJSON["local"].string,
+                                                parte: subJSON["parte"].string,
+                                                disponib: subJSON["disponib"].string,
+                                                localiza: subJSON["localiza"].string,
+                                                dt_empre: subJSON["dt_empre"].string,
+                                                dt_devol: subJSON["dt_devol"].string,
+                                                exemplar: subJSON["exemplar"].string,
+                                                colecao: subJSON["colecao"].string)
+                
+                self.availables.append(available)
+            }
+            callback(self.availables, json.error)
+            
+        }
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
